5d43c7af3015f3d8ff7f60cb2a8da73ab638ef7d
Ryan-Xuan <65810027+Ryan-Xuan@users.noreply.github.com>
2020-06-23 08:27:03 +0800

    Add files via upload

---
+++ sign/create_sign.js
@@ -0,0 +1,169 @@
+
+import request from '../../service/network.js'
+var util = require('../../utils/util.js');
+
+Page({
+    mixins: [require('../../mixin/themeChanged')],
+    data: {
+        array3: ['软件工程', '计算理论', '并行计算','机器学习'],
+        a3:[],
+        value3: 0,       
+        arraylast: ['10', '15','20',  '30','40', '60', '90', ],
+        index: 0,
+        time: '2020/06/22 20:18:39',
+        last:0,
+        toast: false,
+        loading: false,
+        hideToast: false,
+        hideLoading: false,
+        mycourse00:[],
+        got:{
+            latitude:0,
+            longitude:0
+        },
+        inputValue:'',
+        inputdata:'11',
+        cno:'99',
+        cname:'sss',
+        csiname:'a',
+        csignposiL:'0',
+        csignposiW:'0',
+        signtimelast:'10'
+
+
+        
+    },
+
+
+
+
+    getLocation:function(){
+        type:'gcj02'
+        wx.getLocation({
+            success:(res)=>{
+                this.setData({
+                    got:res
+                })
+            },
+          
+        })
+    },
+
+    //获取的课程信息需要数据处理
+    onLoad: function (options) {
+        var time = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据
+        this.setData({
+        time: time.replace(/-/g,"/"),
+        });
+        request({
+          url:'http://10.21.232.109/findalljson'
+        }).then(res =>{ 
+          const mycourse00=res.data
+          this.setData({
+            mycourse00:mycourse00
+          })
+        })
+
+    },
+
+    
+
+    // 发送签到信息失败
+    loginBtnClick: function (e) {
+        this.setData({
+            cname:this.data.array3[this.data.value3],
+            csigntime:this.data.time,
+            csignposiL:this.data.got.longitude,
+            csignposiW:this.data.got.latitude,
+            signtimelast:this.data.arraylast[this.data.last],
+
+        })
+        
+        wx.request({
+            //url: 'http://10.21.232.109/addcsdsign?cno=0&cname='+ this.data.cname+'&csigname='+ this.data.csigname+'&csignposiL='+ this.data.csignposiL+'&csignposiW='+ this.data.csignposiW+'&signtimelast='+ this.data.signtimelast+'&csigned=00&czno=0&csigntime=0',
+            url:'http://10.21.232.109/addcsdsign?cno=0&cname='+ this.data.cname+'&csignposiL='+ this.data.csignposiL+'&csignposiW='+ this.data.csignposiW+'&signtimelast='+ this.data.signtimelast+'&csigned=0&czno=3&csigntime='+ this.data.csigntime,
+            success:function(e){
+              console.log('success')
+            },fail:function(){
+              console.log('fail')
+            }
+          })
+        
+    },
+
+
+
+    
+
+    bindTimeChange: function (e) {
+        this.setData({
+            time: e.detail.value,
+        })
+    },
+
+    bindLastChange: function (e) {
+        this.setData({
+            last: e.detail.value
+        })
+    },
+
+    bindPicker3Change: function(e) {
+        this.setData({
+            value3: e.detail.value           
+        })
+        
+    },
+    bindDateChange: function(e) {
+        this.setData({
+            date: e.detail.value,
+        })
+    },
+    openToast: function() {
+        this.setData({
+            toast: true,
+        });
+        setTimeout(() => {
+            this.setData({
+                hideToast: true
+            });
+            setTimeout(() => {
+                this.setData({
+                    toast: false,
+                    hideToast: false,
+                });
+            }, 300);
+        }, 3000);
+    },
+    openLoading: function() {
+        this.setData({
+            loading: true
+        });
+        setTimeout(() => {
+            this.setData({
+                hideLoading: true
+            });
+            setTimeout(() => {
+                this.setData({
+                    loading: false,
+                    hideLoading: false,
+                });
+            }, 300);
+        }, 3000);
+    },
+
+
+    inputLoc:function(e){
+        this.setData({
+            inputValue:e.detail.value,
+            inputdata:'inputValue',
+        })
+    }
+
+    
+
+
+
+
+
+});
